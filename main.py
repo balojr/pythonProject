4fd24afbdf116a35a99f221c4d2e4179c18370c3
balojr <martinkagendo13@gmail.com>
2024-02-10 14:48:16 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print('hello world')
+
+name = 'martin'
+action = 'coding'
+print(name, action)
+
+print(f'{name} is {action}')
+
+i = 1
+j = 30
+
+for i in range(i, j):
+    if i % 2 == 0:
+        print(i)
+
+
+for i in range(i, j):
+    if not i % 2 == 0:
+        print(i)
+
+list = []
+list.append(True)
+list.append(False)
+list.reverse()
+print(list)
+
